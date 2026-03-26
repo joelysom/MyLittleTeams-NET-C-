7fb5b5d948b9e65e1f3d24eb4c998c5e6a7d6e98
JOELYSOM <joelysomalcantaradasilva@gmail.com>
2026-03-26 14:06:07 -0300

    Version 1.0

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace MeuAppWPF
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Funcionou 🔥");
+        }
+
+        private void NavButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button button)
+            {
+                string tag = button.Tag?.ToString() ?? "";
+                
+                // Oculta todo conteúdo
+                ChatsContent.Visibility = Visibility.Collapsed;
+                TeamsContent.Visibility = Visibility.Collapsed;
+                CalendarContent.Visibility = Visibility.Collapsed;
+                FilesContent.Visibility = Visibility.Collapsed;
+                SettingsContent.Visibility = Visibility.Collapsed;
+
+                // Mostra o conteúdo correspondente
+                switch (tag)
+                {
+                    case "Chats":
+                        ChatsContent.Visibility = Visibility.Visible;
+                        break;
+                    case "Equipes":
+                        TeamsContent.Visibility = Visibility.Visible;
+                        break;
+                    case "Calendario":
+                        CalendarContent.Visibility = Visibility.Visible;
+                        break;
+                    case "Arquivos":
+                        FilesContent.Visibility = Visibility.Visible;
+                        break;
+                    case "Configuracoes":
+                        SettingsContent.Visibility = Visibility.Visible;
+                        break;
+                }
+            }
+        }
+    }
+}
