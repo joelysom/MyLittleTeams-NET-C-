c9f9eeb9735523a5a2cbb3c88ed5941a83f8769a
JOELYSOM <joelysomalcantaradasilva@gmail.com>
2026-04-03 03:01:13 -0300

    novas atualizações gerais

--- obj/Debug/net8.0-windows/MeuApp.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/MeuApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MeuApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f3e081cea03887a0f44cfd37619765b83d0ae3d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cdfbf056597e5496512e8ebbbe0f7a8fb8746dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("MeuApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MeuApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
